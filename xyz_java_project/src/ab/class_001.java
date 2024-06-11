cef9725de2221fa57f1ba7688e5cf7e5b934f1d4
chimni12 <akankshathakare54@gmail.com>
2024-06-12 01:31:55 +0530

    Added two lines

--- xyz_java_project/src/ab/class_001.java
+++ xyz_java_project/src/ab/class_001.java
@@ -3,7 +3,9 @@ package ab;
 public class class_001 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+	System.out.println("hello java");
+	System.out.println("Bye Java");
 
 	}
 
